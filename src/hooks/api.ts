1a8d9d54b23d6a567075c52b95b4d2f66ff07971
Saurav Maharjan <saurav.maharjan7@gmail.com>
2023-10-19 18:37:10 +0545

    feat : added url for comment & posts

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-let baseUrl = "https://www.omdbapi.com/";
-
+// let baseUrl = "https://www.omdbapi.com/";
+let baseUrl = "https://jsonplaceholder.typicode.com/";
 
 export const API = axios.create({baseURL : baseUrl,
 headers:{'content-type':'application/json',}});
